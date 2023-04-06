ca5661e2a4c6f5ca0e000b750cb7d076a4b730ca
lainemarkus <markus-laine@aalto.fi>
2023-04-06 15:37:15 +0300

    edited sign up

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ function App() {
           <p>Kaljakrokettiturnaukseen mahtuu 16 joukkuetta kumpaankin sarjaan. Kerää 4-6 hengen tiimi ja ilmoittaudu mukaan kaljakrokettiin <span className="bold">16.4.2023 mennessä</span>!
           <br/><br/>Kutsuvieraille on oma kiintiö. Mikäli kutsuvieraskiintiöstä jää paikkoja yli, ne täytetään jonosijoille ilmoittautuneilla. 
           <br/><br/>Osallistumismaksut ovat 8€/joukkue. Osallistumismaksulla katetaan sakkojuomat ja krokettivälineet, eli varsinaiset pelijuomat tulee tuoda paikalle itse.
-          <br/><br/><a className="a-link" href="https://athene.fi/ilmo/event/325" rel="noreferrer noopener " target="_blank" >Linkki ilmoittautumiseen</a>
           <p>Peliasu on vapaa mutta suositeltava - pelaajat voivat pukeutua edustamansa järjestön haalareihin taikka pukeutua omaan yhteneväiseen peliasuun.</p>
+          <br/><br/><a className="a-link" href="https://athene.fi/ilmo/event/325" rel="noreferrer noopener " target="_blank" >Linkki ilmoittautumiseen</a>
           </p>
           </div>
        
